0bfeab5b8d8a9972fde71f64e745d847ed815757
Sergey Kosovsky <skosovsky@gmail.com>
2023-12-24 13:41:57 +0300

    Add solutiond for friday

---
+++ friday/go.mod
@@ -0,0 +1,3 @@
+module github.com/skosovsky/go-ya-begin/friday
+
+go 1.21.5
---
+++ friday/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
+	finish := time.Date(3000, 12, 31, 0, 0, 0, 0, time.UTC)
+
+	dayWeeks := map[time.Weekday]int{}
+
+	for i := 0; start != finish; i++ {
+		if start.Day() == 13 {
+			dayWeeks[start.Weekday()]++
+		}
+
+		start = start.Add(time.Hour * 24)
+	}
+
+	fmt.Println(dayWeeks)
+}
