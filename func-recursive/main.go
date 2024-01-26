fb04844cd9ed001e9e0d33b95d6dfb8e7da6fdc7
Sergey Kosovsky <skosovsky@gmail.com>
2024-01-26 21:37:46 +0300

    Add func binSearch

--- func-recursive/main.go
+++ func-recursive/main.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
 	numsSum([]int{1, 1, 1})
+	binSearch([]int{1, 2, 3}, 1)
 }
 
 func numsSum(s []int) int {
@@ -11,4 +12,21 @@ func numsSum(s []int) int {
 
 	return s[0] + numsSum(s[1:])
 	// Берем первый элемент слайса и прибавляем к нему вызов функции с оставшимися элементами
+}
+
+func binSearch(s []int, el int) int {
+	if len(s) == 1 {
+		if s[0] == el {
+			return el
+		}
+		return -1
+	}
+
+	if el > s[:len(s)/2][len(s)/2-1] {
+		res := binSearch(s[len(s)/2:], el)
+		return res
+	} else {
+		res := binSearch(s[:len(s)/2], el)
+		return res
+	}
 }
