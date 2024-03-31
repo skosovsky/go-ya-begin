0000a04accda5c1f6c53fc53d68cff70efc2adde
Sergey Kosovsky <skosovsky@gmail.com>
2024-03-31 08:22:41 +0300

    Finish solution insert-db

--- sprint-8/insert-db/main.go
+++ sprint-8/insert-db/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
 	_ "modernc.org/sqlite"
 )
 
@@ -14,20 +15,27 @@ type DB struct {
 func NewDB() (DB, error) {
 	db, err := sql.Open("sqlite", "demo.db")
 	if err != nil {
-		return DB{}, nil
+		err = fmt.Errorf("db open error: %w", err)
+		return DB{DB: nil}, err
 	}
 
-	return DB{db}, nil
+	return DB{DB: db}, nil
 }
 
 type Client struct {
+	ID       int
 	FIO      string
 	Login    string
 	Birthday string
 	Email    string
 }
 
-func (d DB) insertClient(client Client) error {
+func (c Client) String() string {
+	return fmt.Sprintf("ID: %d, FIO: %s, Login %s, Birthday: %s, Email %s",
+		c.ID, c.FIO, c.Login, c.Birthday, c.Email)
+}
+
+func (d DB) insertClient(client Client) (int, error) {
 	res, err := d.Exec("INSERT INTO clients (fio, login, birthday, email) VALUES (:fio, :login, :birthday, :email)",
 		sql.Named("fio", client.FIO),
 		sql.Named("login", client.Login),
@@ -35,17 +43,20 @@ func (d DB) insertClient(client Client) error {
 		sql.Named("email", client.Email))
 	if err != nil {
 		err = fmt.Errorf("db exec error: %w", err)
-		return err
+		return 0, err
 	}
 
-	log.Println(res.LastInsertId())
-	log.Println(res.RowsAffected())
+	id, err := res.LastInsertId()
+	if err != nil {
+		err = fmt.Errorf("get last insert id error %w: ", err)
+		return 0, err
+	}
 
-	return nil
+	return int(id), nil
 }
 
 func (d DB) updateClientLogin(id int, login string) error {
-	res, err := d.Exec("UPDATE clients SET login = :login WHERE id = :id",
+	_, err := d.Exec("UPDATE clients SET login = :login WHERE id = :id",
 		sql.Named("id", id),
 		sql.Named("login", login))
 	if err != nil {
@@ -53,22 +64,16 @@ func (d DB) updateClientLogin(id int, login string) error {
 		return err
 	}
 
-	log.Println(res.LastInsertId())
-	log.Println(res.RowsAffected())
-
 	return nil
 }
 
 func (d DB) deleteClient(id int) error {
-	res, err := d.Exec("DELETE FROM clients WHERE id = :id",
+	_, err := d.Exec("DELETE FROM clients WHERE id = :id",
 		sql.Named("id", id))
 	if err != nil {
 		err = fmt.Errorf("db exec error: %w", err)
 		return err
 	}
-
-	log.Println(res.LastInsertId())
-	log.Println(res.RowsAffected())
 
 	return nil
 }
@@ -76,11 +81,12 @@ func (d DB) deleteClient(id int) error {
 func (d DB) selectClient(id int) (Client, error) {
 	var client Client
 
-	row := d.QueryRow("SELECT fio, login, birthday, email FROM clients WHERE id = :id",
+	row := d.QueryRow("SELECT id, fio, login, birthday, email FROM clients WHERE id = :id",
 		sql.Named("id", id))
 
-	err := row.Scan(&client.FIO, &client.Login, &client.Birthday, &client.Email)
+	err := row.Scan(&client.ID, &client.FIO, &client.Login, &client.Birthday, &client.Email)
 	if err != nil {
+		err = fmt.Errorf("row scan error: %w", err)
 		return Client{}, err
 	}
 
@@ -93,34 +99,74 @@ func main() {
 		log.Println(err)
 		return
 	}
-	defer db.Close()
+	defer func(db *DB) {
+		err = db.Close()
+		if err != nil {
+			log.Println("db close error")
+		}
+	}(&db)
 
-	newClient := Client{
+	newClient := Client{ //nolint:exhaustruct // ID generated DB
 		FIO:      "Сергей Косовский",
 		Login:    "skosovsky2",
 		Birthday: "19800721",
 		Email:    "skosovsky@gmail.com",
 	}
 
-	err = db.insertClient(newClient)
+	id, err := db.insertClient(newClient)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
-	// TODO: Add method from tasks
+	selected, err := db.selectClient(id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(selected) //nolint:forbidigo // it's learning code
+
+	err = db.updateClientLogin(id, "newLogin")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	selected, err = db.selectClient(id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(selected) //nolint:forbidigo // it's learning code
+
+	err = db.deleteClient(id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	selected, err = db.selectClient(id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(selected) //nolint:forbidigo // it's learning code
 
 	// test func
-	err = insertProduct(db.DB, "Облачное хранилище", 300)
+	err = insertProduct(db.DB, "Облачное хранилище", 300) //nolint:gomnd // it's learning code
 	if err != nil {
 		log.Println(err)
 	}
 
-	err = updatePriceProduct(db.DB, 1, 700)
+	err = updatePriceProduct(db.DB, 1, 700) //nolint:gomnd // it's learning code
 	if err != nil {
 		log.Println(err)
 	}
 
-	err = deleteClient(db.DB, 3)
+	err = deleteClient(db.DB, 3) //nolint:gomnd // it's learning code
 	if err != nil {
 		log.Println(err)
 	}
